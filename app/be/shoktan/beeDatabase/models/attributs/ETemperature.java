576f1aa8d369620a4b48b7c5581d4356c6dbc167
wisthy <wisthy@users.noreply.github.com>
2014-08-05 12:46:46 +0200

    Create ETemperature.java
    enumeration of the temperature for bees

---
+++ app/be/shoktan/beeDatabase/models/attributs/ETemperature.java
@@ -0,0 +1,15 @@
+package be.shoktan.beeDatabase.models.attributs;
+
+/**
+ * enumeration of the temperature for bees
+ * @author Wisthy
+ *
+ */
+public enum ETemperature {
+	icy,
+	cold,
+	normal,
+	warm,
+	hot,
+	hellish;
+}
