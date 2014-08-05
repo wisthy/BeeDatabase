28c47756d0f833916416e6d36537943282a1a952
wisthy <wisthy@users.noreply.github.com>
2014-08-05 11:47:47 +0200

    Create EProductivity.java
    Enumeration to represent the productivity rate of a bee

---
+++ app/be/shoktan/beeDatabase/models/attributs/EProductivity.java
@@ -0,0 +1,16 @@
+package be.shoktan.beeDatabase.models.attributs;
+
+/**
+ * Enumeration to represent the productivity rate of a bee
+ * @author Wisthy
+ *
+ */
+public enum EProductivity {
+	slowest,
+	slower,
+	slow,
+	normal,
+	fast,
+	faster,
+	fastest
+}
