e66c3311ce24c9372efbcdbc24a5d74f8bf6e539
wisthy <wisthy@users.noreply.github.com>
2014-08-05 11:40:42 +0200

    Create EToleranceType.java
    Enumeration to represent the "direction" of a tolerance: up-side, down-side or both-sides

---
+++ app/be/shoktan/beeDatabase/models/attributs/EToleranceType.java
@@ -0,0 +1,13 @@
+package be.shoktan.beeDatabase.models.attributs;
+
+/**
+ * Enumeration to represent the "direction" of a tolerance:
+ * up-side, down-side or both-sides
+ * @author Wisthy
+ *
+ */
+public enum EToleranceType {
+	down,
+	up,
+	both;
+}
