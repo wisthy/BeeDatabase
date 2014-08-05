e8ecb2e5f248cc17653da64a805ee8f262ca5ca8
wisthy <wisthy@users.noreply.github.com>
2014-08-05 11:41:51 +0200

    Create ETolerance.java
    The goal of this enumeration is to represent the range of tolerance of a bee to temperature or humidity

---
+++ app/be/shoktan/beeDatabase/models/attributs/ETolerance.java
@@ -0,0 +1,45 @@
+package be.shoktan.beeDatabase.models.attributs;
+
+/**
+ * 
+ * @author Wisthy
+ * The goal of this enumeration is to represent the range of tolerance 
+ * of a bee to temperature or humidity
+ *
+ */
+public enum ETolerance {
+	none(EToleranceType.both, 0),
+	down_1(EToleranceType.down, 1),
+	down_2(EToleranceType.down, 2),
+	up_1(EToleranceType.up, 1),
+	up_2(EToleranceType.up, 2),
+	both_1(EToleranceType.both, 1),
+	both_2(EToleranceType.both, 2);
+	
+
+	private int value;
+	private EToleranceType type;
+	
+	/**
+	 * @param value
+	 * @param type
+	 */
+	private ETolerance(EToleranceType type, int value) {
+		this.value = value;
+		this.type = type;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public EToleranceType getType() {
+		return type;
+	}
+}
