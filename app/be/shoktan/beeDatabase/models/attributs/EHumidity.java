fdf31a23a37ee313cc6f3c845683de576be6b3cf
wisthy <wisthy@users.noreply.github.com>
2014-08-05 12:47:23 +0200

    Create EHumidity.java
    enumeration of humidy values for bees environment

---
+++ app/be/shoktan/beeDatabase/models/attributs/EHumidity.java
@@ -0,0 +1,12 @@
+package be.shoktan.beeDatabase.models.attributs;
+
+/**
+ * enumeration of humidy values for bees environment
+ * @author Wisthy
+ *
+ */
+public enum EHumidity {
+	damp,
+	normal,
+	arid
+}
