30240076c8c3e65d84914b7fe45d74c0cb551ebe
wisthy <wisthy@users.noreply.github.com>
2014-08-06 09:32:31 +0200

    Create IAttribute.java
    Common interface to be able to manage all the attributes with a single type

---
+++ app/be/shoktan/beeDatabase/models/attributs/IAttribute.java
@@ -0,0 +1,10 @@
+package be.shoktan.beeDatabase.models.attributs;
+
+/**
+ * Common interface to be able to manage all the attributes with a single type
+ * @author Wisthy
+ *
+ */
+public interface IAttribute {
+
+}
