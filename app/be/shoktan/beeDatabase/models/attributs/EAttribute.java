1810c19f3f5e960a1d32aec37f8bd09de5309bf7
wisthy <wisthy@users.noreply.github.com>
2014-08-06 09:31:25 +0200

    Create EAttribute.java
    Enumeration to represent the type of an attribute

---
+++ app/be/shoktan/beeDatabase/models/attributs/EAttribute.java
@@ -0,0 +1,15 @@
+package be.shoktan.beeDatabase.models.attributs;
+
+/**
+ * Enumeration to represent the type of an attribute
+ * @author Wisthy
+ *
+ */
+public enum EAttribute {
+	fertility,
+	humidity,
+	humidityTolerance,
+	temperature,
+	temperatureTolerance,
+	productivity
+}
